28ec43e2f3a63a31cbfc84df4165551227ca8a07
Tung Dang <tungqd90@yahoo.com>
2013-09-21 22:36:32 -0700

    minor change

--- views/notloggedin.php
+++ views/notloggedin.php
@@ -4,10 +4,10 @@
 
 
 <div id="newest" style="height:200px;width:700px;float:left;">
-    <b>Most recent entry</b>
+    <b>Most recent entry<br></b>
 
     <?php 
-    	echo '<br>';
+
     	echo $data['title'].'<br>';
     	echo $data['content'].'<br>';
     	echo $data['name1'].'<br>';
@@ -19,6 +19,8 @@
 </div>
 
 <div id="list" style="height:200px;width:400px;float:right;">
-    Entry list</div>
+    <b>Entry list<br></b>
+    <?php echo $data['title'].'<br>'; ?>
+</div>
 
 

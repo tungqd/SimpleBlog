79d5e0faa7471776b51ef9bd5512eee878d5cbcc
khanhnguyen38 <katenguyenvn@Khanhs-MacBook-Pro.local>
2013-09-21 18:23:12 -0700

    Edit notloggedin and loginscreen

--- views/loginscreen.php
+++ views/loginscreen.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+
 <?php
 /**
 * loginscreen.php
@@ -9,3 +16,17 @@
 *
 */
 ?>
+
+<form name="login">
+    Username<input type="text" name="userid"/>
+    Password<input type="text" name="pw"/>
+    <input type="button" value="Login"/>
+    <input type="reset" value="Cancel"/>
+</form>
+
+
+
+</body>
+</html>
+
+
--- views/notloggedin.php
+++ views/notloggedin.php
@@ -1,2 +1,26 @@
 
-<h2>Welcome to Simple Blog!</h2>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Simple Blog!</title>
+</head>
+<body>
+
+
+<form action="login.php" method="GET">
+    <input type="button" value="Login" >
+</form>
+
+
+<div id="newest" style="height:200px;width:700px;float:left;">
+    <b>Most recent entry</b>
+</div>
+
+<div id="list" style="height:200px;width:400px;float:right;">
+    Entry list</div>
+
+
+
+
+</body>
+</html>

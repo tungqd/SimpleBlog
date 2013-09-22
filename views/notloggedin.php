84dc33772605b06d38bcf946120936376d0fedb2
Tung Dang <tungqd90@yahoo.com>
2013-09-21 22:21:47 -0700

    fix conflict

--- views/notloggedin.php
+++ views/notloggedin.php
@@ -5,8 +5,9 @@
 
 <div id="newest" style="height:200px;width:700px;float:left;">
     <b>Most recent entry</b>
-<<<<<<< HEAD
+
     <?php 
+    	echo '<br>';
     	echo $data['title'].'<br>';
     	echo $data['content'].'<br>';
     	echo $data['name1'].'<br>';
@@ -14,9 +15,7 @@
     	echo $data['name2'].'<br>';
     	echo $data['comment2'].'<br>';
     ?>
-=======
-<?php echo $data["title"];?>
->>>>>>> 3af7ed6dede09e2cf38b2eb2a596964fc0f16051
+
 </div>
 
 <div id="list" style="height:200px;width:400px;float:right;">

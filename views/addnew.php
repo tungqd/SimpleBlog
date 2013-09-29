a664d6ba29aa2ac211e29011cd807767988b2d27
khanhnguyen38 <katenguyenvn@Khanhs-MacBook-Pro.local>
2013-09-28 18:11:08 -0700

    update view

--- views/addnew.php
+++ views/addnew.php
@@ -1,7 +1,8 @@
 <form name="addEntry" action="index.php?c=blog" id="addBlog" method="POST">
 		<input type="hidden" name="ac" value="addEntry">
 		<input type="hidden" name="e" value="<?php echo time(); ?>">
-        Title<input type="text" name="title"/><br>
+        Title: <input type="text" name="title"/><br>
+        Content: <br>
         <textarea rows="4" cols="50" name="content" form="addBlog"></textarea>
     	<input type="submit" value="Submit">
 </form>

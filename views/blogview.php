7d605b4a00ea4bca72f32dae3d281b5444d868e8
khanhnguyen38 <katenguyenvn@Khanhs-MacBook-Pro.local>
2013-09-28 19:26:06 -0700

    update view

--- views/blogview.php
+++ views/blogview.php
@@ -1,64 +1,70 @@
-<div id="newest">
+<div id="Entryview">
+	<div id="blogViewNewest">
     
-    <!-- Display entry itself -->
-	<?php 	
-		$entry = $data[$_GET["a"]];
-    	$blog = $entry[0];
-    	$title = $blog[0];
-    	$content = "";
+    	<!-- Display entry itself -->
+		<?php 	
+			$entry = $data[$_GET["a"]];
+    		$blog = $entry[0];
+    		$title = $blog[0];
+    		$content = "";
     	
-    	for ($i = 1; $i < count($blog); $i++)
-    	{
-    		$content .= $blog[$i] . "<br>";
-    	}
+    		for ($i = 1; $i < count($blog); $i++)
+    		{
+    			$content .= $blog[$i] . "<br>";
+    		}
     	
-    	echo $title;
-    	echo "<br><br>";
-    	echo $content."<br>";
-    ?> 
+    		echo $title;
+    		echo "<br><br>";
+    		echo $content."<br>";
+    	?> 
+	</div>
+
+
+
+	<div id="blogViewComment">
+    	<b>Comments</b><br>
+    
+    	<!-- Display array of comments-->
+   		<?php
+    		for ($i = 1; $i < count($entry); $i++)
+    		{
+    			$comment_array = $entry[$i];
+    			$comment_name = $comment_array[0];
+    			$comment_content = "";
+    		
+    			/* Add and append content line in EACH comment */
+    			for ($j = 1; $j < count($comment_array); $j++)
+    			{
+    				$comment_content .= $comment_array[$j] . "<br>";
+       			}	   
+       	
+       			echo $comment_name;
+    			echo "<br>";
+    			echo $comment_content;
+    	?>
+    			<!-- "Delete" comment button -->
+    			<form action="index.php" method="GET"> 
+					<input type="hidden" name="c" value="main">
+					<input type="hidden" name="view" value="notloggedin">
+    				<input type="submit" value="Delete">
+				</form>
+   		<?php
+        	}
+    	?>
+	</div>
 </div>
 
-<div id="blogViewComment">
-    <b>Comments</b><br>
-    
-    <!-- Display array of comments-->
-    <?php
-    	for ($i = 1; $i < count($entry); $i++)
-    	{
-    		$comment_array = $entry[$i];
-    		$comment_name = $comment_array[0];
-    		$comment_content = "";
-    		
-    		/* Add and append content line in EACH comment */
-    		for ($j = 1; $j < count($comment_array); $j++)
-    		{
-    			$comment_content .= $comment_array[$j] . "<br>";
-       		}	   
-       	
-       		echo $comment_name;
-    		echo "<br>";
-    		echo $comment_content;
-    ?>
-    		<!-- "Delete" comment button -->
-    		<form action="index.php" method="GET"> 
-				<input type="hidden" name="c" value="main">
-				<input type="hidden" name="view" value="notloggedin">
-    			<input type="submit" value="Delete">
-			</form>
-    <?php
-        	}
-    ?>
-</div>
+
 <div id="addComment">
     <!-- Add comment section -->
     <form name="add" action="index.php?c=main" method="POST">
         Name<input type="text" name="name"/><br>
-        Comment<input type="text" name="comment"/>
+        Content: <br>
+        <textarea rows="4" cols="50" name="content" form="addBlog"></textarea>
         	<input type="hidden" name="ac" value="addComment">
         	<input type="hidden" name="e" value="<?php echo $_GET["a"]; ?>">
 			<input type="hidden" name="t" value="<?php echo time(); ?>">
     		<input type="submit" value="Submit comment">
     	</form>
-    </form>
-    
+    </form>    
 </div>

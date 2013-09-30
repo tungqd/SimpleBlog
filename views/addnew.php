a125beb5a2b82d7857a5f5e3d3bb0201c1330398
khanhnguyen38 <katenguyenvn@Khanhs-MacBook-Pro.local>
2013-09-30 14:52:36 -0700

    minor

--- views/addnew.php
+++ views/addnew.php
@@ -1,9 +1,9 @@
 
-<form name="addEntry" action="index.php?c=blog" id="addBlog" method="POST">
+<form id="addNewEntry" name="addEntry" action="index.php?c=blog" id="addBlog" method="POST">
 		<input type="hidden" name="ac" value="addEntry">
 		<input type="hidden" name="e" value="<?php echo time(); ?>">
         Title: <input type="text" name="title"/><br>
         Content: <br>
-        <textarea rows="4" cols="50" name="content" form="addBlog"></textarea>
+        <textarea id="newEntryTextArea" name="content" form="addBlog"></textarea>
     	<input type="submit" value="Submit">
 </form>

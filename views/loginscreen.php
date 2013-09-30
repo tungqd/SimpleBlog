7d66c8950f8ca8813b2666ea1064e4e902881263
khanhnguyen38 <katenguyenvn@Khanhs-MacBook-Pro.local>
2013-09-30 13:56:24 -0700

    minor

--- views/loginscreen.php
+++ views/loginscreen.php
@@ -6,27 +6,29 @@ if (isset($_SESSION["Usage"]) && $_SESSION["Usage"] == "error") {
 	echo "Invalid username/password. Please try again";
 }
 ?>
-<div class="LoginScreenView">
 
-<div id="LoginArea">
+<div class="wrapper">
 
-	<!-- "Login" button-->
-	<form name="login" action="index.php?c=login" method="POST">  
-		<input type="hidden" name="ac" value="log">
-    	Username: <input type="text" name="userid"><br>
-    	Password: <input type="password" name="pw"><br>
-    	<input type="submit" value="Login">
-	</form> 
+	<div id="LoginArea">
 
-</div> 
+		<!-- "Login" button-->
+		<form name="login" action="index.php?c=login" method="POST">  
+			<input type="hidden" name="ac" value="log">
+    		Username: <input type="text" name="userid"><br>
+    		Password: <input type="password" name="pw"><br>
+    		<input type="submit" value="Login">
+		</form> 
 
-<div id="CancelButton">
-	<!-- "Cancel" button: bring user back to homepage -->
-	<form name="cancel" action="index.php" method="GET">
-		<input type="hidden" name="c" value="main">
-		<input type="hidden" name="view" value="notloggedin">
-		<input type="submit" value="Cancel">
-	</form>
-</div> 
+	</div> 
+
+	<div id="CancelButton">
+	
+		<!-- "Cancel" button: bring user back to homepage -->
+		<form name="cancel" action="index.php" method="GET">
+			<input type="hidden" name="c" value="main">
+			<input type="hidden" name="view" value="notloggedin">
+			<input type="submit" value="Cancel">
+		</form>
+	</div> 
 
 </div>
